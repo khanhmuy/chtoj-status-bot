72ccc565a1ae1e6e3c83b091b6637e1c6d44a7d0
khanhmuy <maikhanhhuy0608@outlook.com>
2024-09-16 13:46:59 +0700

    tf am i even doing

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //Import node modules
 const { Client, GatewayIntentBits, Partials, WebhookClient, EmbedBuilder} = require('discord.js');
 const chalk = require('chalk');
-const Monitor = require('ping-monitor');
 require('dotenv').config();
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(() => resolve(), ms));
@@ -27,11 +26,11 @@ client.login(client.token);
 
 client.on('ready', () => {
     console.log(chalk.hex('#a6e3a1').underline('Bot is ready!'));
-    let prevStt = true;
+    let prevStt = false;
     let status = null;
     async function check() {
         try {
-            const res = await fetch('https://oj.thptchuyenhatinh.edu.vn');
+            const res = await fetch(process.env.SITE_URL);
             if (res.ok == true) {
                 status = true;
             } else {
@@ -43,7 +42,7 @@ client.on('ready', () => {
     }
     
     function sendNotif() {
-        const sendChannel = client.channels.cache.get('1284868089640517765');
+        const sendChannel = client.channels.cache.get(process.env.SEND_CHANNEL);
         if (status == false && prevStt == true) {
             const timeStamp = Math.floor(Date.now() / 1000);
             const downEmbed = new EmbedBuilder()
@@ -57,13 +56,13 @@ client.on('ready', () => {
             const timeStamp = Math.floor(Date.now() / 1000);
             const restoredEmbed = new EmbedBuilder()
                 .setTitle('CHTOJ is RESTORED!')
-                .setDescription(`CHTOJ is RESTORED!\n\n from <t:${timeStamp}:R>`)
+                .setDescription(`CHTOJ is UP!\n\n from <t:${timeStamp}:R>`)
                 .setColor('#a6e3a1')
                 .setTimestamp();
             sendChannel.send({embeds: [restoredEmbed], content: `<@&1284871505632821269>`});
             prevStt = true;
         }
     }
-    setInterval(check, 5000);
-    setInterval(sendNotif, 2000);
+    setInterval(check, 30000);
+    setInterval(sendNotif, 30000);
 });
